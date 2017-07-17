a82271ac11d181fbe3db8657577962be3c45fefd
seefo <robbie.uvanni@gmail.com>
2017-07-17 14:40:21 -0700

    Added license to source files

--- triggers/hud.js
+++ triggers/hud.js
@@ -1,3 +1,13 @@
+/*
+//  hud.js
+//
+//  Created by Robbie Uvanni on 2017-07-17
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
 (function() {
   var APP_NAME = "ZONE EDITOR";
   var APP_URL  = Script.resolvePath("./hud/page.html");
--- triggers/modules/sound.js
+++ triggers/modules/sound.js
@@ -1,3 +1,13 @@
+/*
+//  sound.js
+//
+//  Created by Robbie Uvanni on 2017-07-17
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
 /* userdata:
     audio_url,
     position_uuid
