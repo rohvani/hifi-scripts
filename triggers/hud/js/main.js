29ceada53a9eafc7fe62a8973873bf4056593a7f
seefo <robbie.uvanni@gmail.com>
2017-07-17 16:25:40 -0700

    Added a dropdown/select hackfix until they're fixed

--- triggers/hud/js/main.js
+++ triggers/hud/js/main.js
@@ -17,10 +17,17 @@ var data = {
 
 var controller = {
   // called when we select a trigger type to customize
-  selectAction: function(e, model) {
-    data.selectedAction = data.actions.filter(function(element){ 
+  selectAction: function(e, model) {   
+    // commented out until hack for dropdowns is fixed
+    /*data.selectedAction = data.actions.filter(function(element){ 
       return element.name === data.selectedActionName;
+    })[0];*/
+    
+    // use dropdown hack
+    data.selectedAction = data.actions.filter(function(element){ 
+      return element.name === $("#selectedAction").find("span")[0].innerHTML;
     })[0];
+    // end hack
   },
   // called when we click 'Create' button for a trigger
   createTrigger: function(e, model) {
@@ -53,6 +60,98 @@ var controller = {
 function print(message) {
   $("#output").append('> ' + message + '<br>');
 }
+
+// hack for dropdowns/select, needs to be removed once they are fixed
+function replaceDropdowns() {
+  function setDropdownText(dropdown) {
+        var lis = dropdown.parentNode.getElementsByTagName("li");
+        var text = "";
+        for (var i = 0; i < lis.length; i++) {
+            if (String(lis[i].getAttribute("value")) === String(dropdown.value)) {
+                text = lis[i].textContent;
+            }
+        }
+        dropdown.firstChild.textContent = text;
+    }
+
+    function toggleDropdown(event) {
+        var element = event.target;
+        if (element.nodeName !== "DT") {
+            element = element.parentNode;
+        }
+        element = element.parentNode;
+        var isDropped = element.getAttribute("dropped");
+        element.setAttribute("dropped", isDropped !== "true" ? "true" : "false");
+    }
+
+    function setDropdownValue(event) {
+        var dt = event.target.parentNode.parentNode.previousSibling;
+        dt.value = event.target.getAttribute("value");
+        dt.firstChild.textContent = event.target.textContent;
+
+        dt.parentNode.setAttribute("dropped", "false");
+
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent("change", true, true);
+        dt.dispatchEvent(evt);
+    }
+    
+  try {
+
+    var elDropdowns = document.getElementsByTagName("select");
+    for (var i = 0; i < elDropdowns.length; i++) {
+        var options = elDropdowns[i].getElementsByTagName("option");
+        var selectedOption = 0;
+        for (var j = 0; j < options.length; j++) {
+            if (options[j].getAttribute("selected") === "selected") {
+                selectedOption = j;
+            }
+        }
+        var div = elDropdowns[i].parentNode;
+
+        var dl = document.createElement("dl");
+        div.appendChild(dl);
+
+        var dt = document.createElement("dt");
+        dt.name = elDropdowns[i].name;
+        dt.id = elDropdowns[i].id;
+        dt.addEventListener("click", toggleDropdown, true);
+        dl.appendChild(dt);
+
+        var span = document.createElement("span");
+        span.setAttribute("value", options[selectedOption].value);
+        span.textContent = options[selectedOption].firstChild.textContent;
+        dt.appendChild(span);
+
+        var span = document.createElement("span");
+        span.textContent = "5"; // caratDn
+        dt.appendChild(span);
+
+        var dd = document.createElement("dd");
+        dl.appendChild(dd);
+
+        var ul = document.createElement("ul");
+        dd.appendChild(ul);
+
+        for (var j = 0; j < options.length; j++) {
+            var li = document.createElement("li");
+            li.setAttribute("value", options[j].value);
+            li.textContent = options[j].firstChild.textContent;
+            li.addEventListener("click", setDropdownValue);
+            ul.appendChild(li);
+        }
+    }
+
+    elDropdowns = document.getElementsByTagName("select");
+    while (elDropdowns.length > 0) {
+        var el = elDropdowns[0];
+        el.parentNode.removeChild(el);
+        elDropdowns = document.getElementsByTagName("select");
+    }
+  } catch(e) { print(e); }
+}
+// end hack
+
 
 function main() {
   
@@ -111,5 +210,9 @@ function main() {
     data: data,
     controller: controller
   });
+
+  // temporary hack for dropdowns, comes from entityProperties.js:1550
+  setInterval(function() { replaceDropdowns(); }, 200);
+  // end hack
 }
 $(document).ready(main);
