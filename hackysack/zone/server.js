939074170a04312e6e036cc0163a63ca13efecec
seefo <robbie.uvanni@gmail.com>
2017-07-26 17:29:37 -0700

    Updated how scoreboard works

--- hackysack/zone/server.js
+++ hackysack/zone/server.js
@@ -43,26 +43,29 @@
     "shapeType": "sphere",
     "type": "Model"
   };
-  var HACKY_SACK_SCOREBOARD = {
-     "backgroundColor": { "blue": 64, "green": 64, "red": 64 },
-     "dimensions": { "x": 2.0, "y": 1.0, "z": 0.01 },
-     "text": "",
-     "type": "Text",
-     "userData": "{\"grabbableKey\":{\"grabbable\":false}}"
-  };
+  
+  var HACKY_SACK_SCOREBOARD_SPAWN_OFFSET = { x: 0.0, y: 0.0, z: -5.0 };
+  var HACKY_SACK_SCOREBOARD_JSON = Script.resolvePath("./../objects/scoreboard.json");
+  var HACKY_SACK_SCOREBOARD_PART_NAMES = [
+    "highScoreText",
+    "highPersonalStreak",
+    "scoreText",
+    "multiplierText",
+    "billboard"
+  ];
   
   var _this;
   var updateInterval;
   
   var hackySack;
-  var scoreboard;
+  var scoreboard = { };
   
   var lastHits = { };
   var lastHitter = "";
   
   var score = 0;
   var scoreMultiplier = 0;
-  var longestPersonalStreak = 0;
+  var personalStreak = 0;
   var longestPassStreak = 0;
   
   function createHackySack() {
@@ -81,18 +84,40 @@
     return distance > HACKY_SACK_MAX_DISTANCE;
   }
   
+  function copyVector(v) {
+    return { x:v.x, y:v.y, z:v.z };
+  }
+  
   function createScoreboard() {
-    HACKY_SACK_SCOREBOARD.position = _this.getPosition();
-    scoreboard = Entities.addEntity(HACKY_SACK_SCOREBOARD);
+    var entities = Script.require(HACKY_SACK_SCOREBOARD_JSON).Entities;
+    var spawnOffset = Vec3.sum(_this.getPosition(), HACKY_SACK_SCOREBOARD_SPAWN_OFFSET);
+    
+    for (var i = 0; i < entities.length; i++) {
+      // copy the position in case of a reset since the json will be cached
+      // and we don't want to permanentely modify the json
+      var originalPosition = copyVector(entities[i].position);
+      var name = entities[i].name;
+      
+      entities[i].position = Vec3.sum(entities[i].position, spawnOffset);
+      var entity = Entities.addEntity(entities[i]);
+      
+      // restore original position so that billboard will respawn in same
+      // location
+      entities[i].position = originalPosition;
+      
+      if (name) {
+        if (arrayContainsElement(HACKY_SACK_SCOREBOARD_PART_NAMES, name)) {
+          scoreboard[entities[i].name] = entity;
+        }
+      }
+    }
   }
   
   function updateLeaderboard() {
-    Entities.editEntity(scoreboard, {
-      text: "Score:                       " + score + "\n" +
-            "Multiplier:                  " + scoreMultiplier + "\n" +
-            "Longest Personal Streak:     " + longestPersonalStreak + "\n" +
-            "Longest Pass Streak:         " + longestPassStreak
-    });
+    Entities.editEntity(scoreboard.scoreText, { text: score });
+    Entities.editEntity(scoreboard.multiplierText, { text: scoreMultiplier });
+    Entities.editEntity(scoreboard.highScoreText, { text: getHighScore() });
+    Entities.editEntity(scoreboard.highPersonalStreak, { text: getHighPersonalStreak() });
   }
   
   function resetGame() {
@@ -101,7 +126,7 @@
     
     score = 0;
     scoreMultiplier = 0;
-    longestPersonalStreak = 0;
+    personalStreak = 0;
     longestPassStreak = 0;
     
     Entities.deleteEntity(hackySack);
@@ -112,8 +137,49 @@
     return userdata.highscore ? userdata.highscore : 0;
   }
   
+  function getUserData() {
+    return JSON.parse(Entities.getEntityProperties(_this.entityID).userData);
+  }
+  
+  function setHighScore(newScore) {
+    if (newScore > getHighScore()) {
+      var userdata = getUserData();
+      userdata.highscore = newScore;
+      Entities.editEntity(_this.entityID, { userData: JSON.stringify(userdata) });
+    }
+  }
+  
+  function getHighPersonalStreak() {
+    var userdata = JSON.parse(Entities.getEntityProperties(_this.entityID).userData);
+    return userdata.highstreak ? userdata.highstreak : 0;
+  }
+  
+function setHighPersonalStreak(newStreak) {
+    
+    print("updating streak " + newStreak + " " + getHighPersonalStreak());
+    
+    if (newStreak > getHighPersonalStreak()) {
+      var userdata = getUserData();
+      userdata.highstreak = newStreak;
+      Entities.editEntity(_this.entityID, { userData: JSON.stringify(userdata) });
+    }
+  }
+  
   function getLastHitTime() {
     return lastHitter.length > 0 ? lastHits[lastHitter] : 0;
+  }
+
+  function getNameForEntity(entityID) {
+    return Entities.getEntityProperties(entityID, ["name"]).name;
+  }
+  
+  function arrayContainsElement(array, element) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        return true;
+      }
+    }
+    return false;
   }
   
   function update() {
@@ -127,7 +193,7 @@
     var data = JSON.parse(message);
     
     if (data.id !== hackySack) {
-      print("Dropping message due to hackysack ID mismatch: got " + data.id + ", expected " + hackySack);
+      //print("Dropping message due to hackysack ID mismatch: got " + data.id + ", expected " + hackySack);
       return;
     }
     
@@ -139,16 +205,19 @@
           
           if (data.hitter !== lastHitter) {
             scoreMultiplier++;
-            longestPersonalStreak = 1;
+            personalStreak = 1;
             longestPassStreak++;
           } else {
-            longestPersonalStreak++;
+            personalStreak++;
             longestPassStreak = 1;
           }
-          
+        
           score += scoreMultiplier;
           lastHitter = data.hitter;
           print("New score: " + score);
+          
+          setHighPersonalStreak(personalStreak);
+          setHighScore(score);
         }
         break;
         
@@ -179,7 +248,13 @@
     
     unload: function() { 
       Script.clearInterval(updateInterval);
-      Entities.deleteEntity(scoreboard);
+      
+      // clean up all scoreboard components
+      for (var i = 0; i < HACKY_SACK_SCOREBOARD_PART_NAMES.length; i++) {
+        var name = HACKY_SACK_SCOREBOARD_PART_NAMES[i];
+        Entities.deleteEntity(scoreboard[name]);
+      }
+      
       Entities.deleteEntity(hackySack);
     },
     
