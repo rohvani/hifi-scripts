25a0b10f514f8763feff9eb666120cecc915ac8f
seefo <robbie.uvanni@gmail.com>
2017-07-27 11:39:37 -0700

    Added lower leg and upper leg colliders

--- hackysack/zone/client.js
+++ hackysack/zone/client.js
@@ -4,6 +4,8 @@
 //  Created by Robbie Uvanni on 2017-07-24
 //  Copyright 2017 High Fidelity, Inc.
 //
+//  Original hackysack colliders script by Thijs Wenker
+//
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 */
@@ -16,7 +18,7 @@
   var _this;
   var rezzedEntities = [];
   
-  function addLegColliders(leftSide) {
+  function addFootColliders(leftSide) {
     var jointPrefix = leftSide ? 'Left' : 'Right';
     var footJointName = jointPrefix + 'Foot';
     var footJointIndex = MyAvatar.getJointIndex(footJointName);
@@ -34,7 +36,7 @@
         parentID: MyAvatar.sessionUUID,
         parentJointIndex: footJointIndex,
         friction: 1.0, // full foot friction
-        dimensions: { x: 0.1, y: 0.1, z: 0.275 }, //dimensions for sphere
+        dimensions: { x: 0.1, y: 0.1, z: 0.275 },
         lifetime: LIFETIME,
         visible: COLLIDERS_VISIBLE,
         userData: JSON.stringify({ ownerID: MyAvatar.sessionUUID })
@@ -42,10 +44,62 @@
 
     rezzedEntities.push(footEntity);
   }
+  
+  function addLegColliders(leftSide) {
+    var jointPrefix = leftSide ? 'Left' : 'Right';
+    var jointName = jointPrefix + 'Leg';
+    var jointIndex = MyAvatar.getJointIndex(jointName);
+    
+    if (jointIndex === -1) {
+        print('Could not find ' + jointName);
+        return;
+    }
+    
+    var entity = Entities.addEntity({
+        name: 'HackySack Collider',
+        type: 'Sphere',
+        localPosition: {x: 0, y: 0.25, z: -0.025},
+        localRotation: Quat.fromPitchYawRollDegrees(0,0,0),
+        parentID: MyAvatar.sessionUUID,
+        parentJointIndex: jointIndex,
+        friction: 1.0, // full foot friction
+        dimensions: { x: 0.1, y: 0.5, z: 0.1 },
+        lifetime: LIFETIME,
+        visible: COLLIDERS_VISIBLE,
+        userData: JSON.stringify({ ownerID: MyAvatar.sessionUUID })
+    });
 
-  function HackySackClient() {
-    _this = this;
+    rezzedEntities.push(entity);
   }
+  
+  function addUpLegColliders(leftSide) {
+    var jointPrefix = leftSide ? 'Left' : 'Right';
+    var jointName = jointPrefix + 'UpLeg';
+    var jointIndex = MyAvatar.getJointIndex(jointName);
+    
+    if (jointIndex === -1) {
+        print('Could not find ' + jointName);
+        return;
+    }
+    
+    var entity = Entities.addEntity({
+        name: 'HackySack Collider',
+        type: 'Box',
+        localPosition: {x: 0, y: 0.2, z: 0},
+        localRotation: Quat.fromPitchYawRollDegrees(0,0,0),
+        parentID: MyAvatar.sessionUUID,
+        parentJointIndex: jointIndex,
+        friction: 1.0, // full foot friction
+        dimensions: { x: 0.08, y: 0.4, z: 0.1 },
+        lifetime: LIFETIME,
+        visible: COLLIDERS_VISIBLE,
+        userData: JSON.stringify({ ownerID: MyAvatar.sessionUUID })
+    });
+
+    rezzedEntities.push(entity);
+  }
+
+  function HackySackClient() { _this = this; }
 
   HackySackClient.prototype = {    
     preload: function(id) { _this.entityID = id; },
@@ -53,8 +107,14 @@
     unload: function() { },
     
     enterEntity: function(entityID) {
+      addFootColliders(true);
+      addFootColliders(false);
+      
       addLegColliders(true);
       addLegColliders(false);
+      
+      addUpLegColliders(true);
+      addUpLegColliders(false);
     },
     
     leaveEntity: function(entityID) {
